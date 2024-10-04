6a364f100bc6edf103b4e9566faaa5b733e0cd34
Rai Sacramento <raisacramento007@gmail.com>
2024-10-04 19:34:11 -0300

    Encapsulamento
    C#

---
+++ Pessoa.cs
@@ -0,0 +1,64 @@
+﻿namespace Encapsulamento
+{
+    public class Pessoa
+    {
+        private string nome;
+        private int idade;
+        private string telefone;
+        private string email;
+        private string endereco;
+
+        public string Nome
+        {
+            get { return nome; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value)) { Console.WriteLine("Por favor digite o seu nome."); }
+                else { nome = value; }
+            }
+        }
+        public int Idade
+        {
+            get { return idade; }
+            set
+            {
+                if (value < 0) { Console.WriteLine("Digite sua idade correta."); }
+                else { idade = value; }
+            }
+        }
+        public string Endereco
+        {
+            get { return endereco; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value)) { Console.WriteLine("Por favor digite o seu endereço."); }
+                else { endereco = value; }
+            }
+        }
+        public string Telefone
+        {
+            get { return telefone; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value)) { Console.WriteLine("Por favor digite o seu telefone."); }
+                else { telefone = value; }
+            }
+        }
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value)) { Console.WriteLine("Por favor digite o seu email.\n"); }
+                else { email = value; }
+            }
+        }
+
+    }
+}
+
+
+
+  
+        
+        
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.Nome = "";
+            pessoa.Idade =  0;
+            pessoa.Endereco = "";
+            pessoa.Telefone = "";
+            pessoa.Email = "";
+
+            pessoa.Nome = "Rai";
+            pessoa.Idade = 27;
+            pessoa.Endereco = "Rua um";
+            pessoa.Telefone = "819123456";
+            pessoa.Email = "rai@rai.com";
+
+            Console.WriteLine(pessoa.Nome);
+            Console.WriteLine(pessoa.Idade + " anos");
+            Console.WriteLine(pessoa.Endereco);
+            Console.WriteLine(pessoa.Telefone);
+            Console.WriteLine(pessoa.Email);    
+
+
+
+
+        }
+    }
+}
+
